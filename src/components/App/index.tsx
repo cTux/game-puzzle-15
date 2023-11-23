5372dfd3e6c28938229f4b1645ee754bd011810f
Viacheslav Miroshko <cccTux@gmail.com>
2023-11-23 16:06:13 +0200

    Initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,22 @@
+import cns from 'clsx';
+import { useCallback, useState } from 'react';
+
+import { Button } from '~/components/Button';
+import { Counter } from '~/components/Counter';
+
+import cn from './styles.module.scss';
+
+export const App = () => {
+  const [value, setValue] = useState<number>(
+    +import.meta.env.VITE_COUNTER || 0
+  );
+
+  const handleOnClick = useCallback(() => setValue((value) => value + 1), []);
+
+  return (
+    <div id={'app'} className={cns(cn.app)}>
+      <Counter value={value} />
+      <Button onClick={handleOnClick} />
+    </div>
+  );
+};
